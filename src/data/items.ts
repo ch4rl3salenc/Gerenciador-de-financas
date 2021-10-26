798b9954e57f66066ed3819b48f17c801557b3fa
ch4rl3salenc <ch4rl3s@gmail.com>
2021-10-26 15:28:05 -0300

    Initial commit

---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from '../types/Item';
+
+export const items: Item[] = [
+    { date: new Date(2021, 9, 6), category: 'food', title: 'Restaurante', value: 35.52 },
+    { date: new Date(2021, 9, 15), category: 'food', title: 'Mercado', value: 29 },
+    { date: new Date(2021, 9, 16), category: 'rent', title: 'Aluguel', value: 2300 },
+    { date: new Date(2021, 10, 18), category: 'salary', title: 'Salário', value: 4000 },
+];
